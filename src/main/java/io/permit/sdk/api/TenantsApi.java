5174910d1c9e577274081729e955da5e55bc6d63
Asaf Cohen <asaf@permit.io>
2023-05-14 12:42:18 +0300

    more javadoc

--- src/main/java/io/permit/sdk/api/TenantsApi.java
+++ src/main/java/io/permit/sdk/api/TenantsApi.java
@@ -27,11 +27,26 @@ interface ITenantsApi {
     void delete(String tenantKey) throws IOException, PermitApiError, PermitContextError;
 }
 
+/**
+ * The TenantsApi class provides methods for interacting with tenants using the Permit REST API.
+ */
 public class TenantsApi extends BaseApi implements ITenantsApi {
+    /**
+     * Constructs a new TenantsApi instance with the specified OkHttpClient and PermitConfig.
+     *
+     * @param client The OkHttpClient instance to be used for making HTTP requests.
+     * @param config The PermitConfig instance that contains the SDK configuration.
+     */
     public TenantsApi(OkHttpClient client, PermitConfig config) {
         super(client, config, LoggerFactory.getLogger(TenantsApi.class));
     }
 
+    /**
+     * Constructs the api URLs for the Tenants API.
+     *
+     * @param url The URL fragment.
+     * @return The formatted URL for the Tenants API.
+     */
     private String getTenantsUrl(String url) {
         return buildUrl(
                 String.format(
@@ -43,6 +58,16 @@ public class TenantsApi extends BaseApi implements ITenantsApi {
         );
     }
 
+    /**
+     * Retrieves a paginated list of tenants.
+     *
+     * @param page    The page number of the result set to retrieve.
+     * @param perPage The number of items per page.
+     * @return An array of TenantRead objects representing the retrieved tenants.
+     * @throws IOException           If an I/O error occurs during the HTTP request.
+     * @throws PermitApiError        If an error occurs in the Permit API.
+     * @throws PermitContextError    If there is an error in the Permit context.
+     */
     public TenantRead[] list(int page, int perPage) throws IOException, PermitApiError, PermitContextError {
         ensureContext(ApiKeyLevel.ENVIRONMENT_LEVEL_API_KEY);
         String url = getTenantsUrl("");
@@ -64,14 +89,42 @@ public class TenantsApi extends BaseApi implements ITenantsApi {
         }
     }
 
+    /**
+     * Retrieves a paginated list of tenants with the default number of items per page.
+     *
+     * @param page The page number of the result set to retrieve.
+     * @return An array of TenantRead objects representing the retrieved tenants.
+     * @throws IOException           If an I/O error occurs during the HTTP request.
+     * @throws PermitApiError        If an error occurs in the Permit API.
+     * @throws PermitContextError    If there is an error in the Permit context.
+     */
     public TenantRead[] list(int page) throws IOException, PermitApiError, PermitContextError {
         return this.list(page, 100);
     }
 
+    /**
+     * Retrieves a list of tenants with default pagination.
+     *
+     * @return An array of TenantRead objects representing the retrieved tenants.
+     * @throws IOException           If an I/O error occurs during the HTTP request.
+     * @throws PermitApiError        If an error occurs in the Permit API.
+     * @throws PermitContextError    If there is an error in the Permit context.
+     */
     public TenantRead[] list() throws IOException, PermitApiError, PermitContextError {
         return this.list(1);
     }
 
+    /**
+     * Retrieves a paginated result of users for the specified tenant.
+     *
+     * @param tenantKey The key of the tenant.
+     * @param page      The page number
+     * @param perPage The number of items per page.
+     * @return A PaginatedResultUserRead object representing the retrieved paginated result of users.
+     * @throws IOException           If an I/O error occurs during the HTTP request.
+     * @throws PermitApiError        If an error occurs in the Permit API.
+     * @throws PermitContextError    If there is an error in the Permit context.
+     */
     public PaginatedResultUserRead listTenantUsers(String tenantKey, int page, int perPage) throws IOException, PermitApiError, PermitContextError {
         ensureContext(ApiKeyLevel.ENVIRONMENT_LEVEL_API_KEY);
         String url = getTenantsUrl(String.format("/%s/users", tenantKey));
@@ -90,14 +143,42 @@ public class TenantsApi extends BaseApi implements ITenantsApi {
         return this.<PaginatedResultUserRead>callApiAndParseJson(request, PaginatedResultUserRead.class);
     }
 
+    /**
+     * Retrieves a paginated result of users for the specified tenant with the specified page number.
+     *
+     * @param tenantKey The key of the tenant.
+     * @param page      The page number of the result set to retrieve.
+     * @return A PaginatedResultUserRead object representing the retrieved paginated result of users.
+     * @throws IOException           If an I/O error occurs during the HTTP request.
+     * @throws PermitApiError        If an error occurs in the Permit API.
+     * @throws PermitContextError    If there is an error in the Permit context.
+     */
     public PaginatedResultUserRead listTenantUsers(String tenantKey, int page) throws IOException, PermitApiError, PermitContextError {
         return this.listTenantUsers(tenantKey, page, 100);
     }
 
+    /**
+     * Retrieves a paginated result of users for the specified tenant with default pagination.
+     *
+     * @param tenantKey The key of the tenant.
+     * @return A PaginatedResultUserRead object representing the retrieved paginated result of users.
+     * @throws IOException           If an I/O error occurs during the HTTP request.
+     * @throws PermitApiError        If an error occurs in the Permit API.
+     * @throws PermitContextError    If there is an error in the Permit context.
+     */
     public PaginatedResultUserRead listTenantUsers(String tenantKey) throws IOException, PermitApiError, PermitContextError {
         return this.listTenantUsers(tenantKey, 1);
     }
 
+    /**
+     * Retrieves a tenant by its key.
+     *
+     * @param tenantKey The key of the tenant.
+     * @return A TenantRead object representing the retrieved tenant.
+     * @throws IOException           If an I/O error occurs during the HTTP request.
+     * @throws PermitApiError        If an error occurs in the Permit API.
+     * @throws PermitContextError    If there is an error in the Permit context.
+     */
     public TenantRead get(String tenantKey) throws IOException, PermitApiError, PermitContextError {
         ensureContext(ApiKeyLevel.ENVIRONMENT_LEVEL_API_KEY);
         String url = getTenantsUrl(String.format("/%s", tenantKey));
@@ -110,14 +191,41 @@ public class TenantsApi extends BaseApi implements ITenantsApi {
         return this.<TenantRead>callApiAndParseJson(request, TenantRead.class);
     }
 
+    /**
+     * Retrieves a tenant by its key.
+     *
+     * @param tenantKey The key of the tenant.
+     * @return A TenantRead object representing the retrieved tenant.
+     * @throws IOException           If an I/O error occurs during the HTTP request.
+     * @throws PermitApiError        If an error occurs in the Permit API.
+     * @throws PermitContextError    If there is an error in the Permit context.
+     */
     public TenantRead getByKey(String tenantKey) throws IOException, PermitApiError, PermitContextError {
         return this.get(tenantKey);
     }
 
+    /**
+     * Retrieves a tenant by its ID.
+     *
+     * @param tenantId The ID of the tenant.
+     * @return A TenantRead object representing the retrieved tenant.
+     * @throws IOException           If an I/O error occurs during the HTTP request.
+     * @throws PermitApiError        If an error occurs in the Permit API.
+     * @throws PermitContextError    If there is an error in the Permit context.
+     */
     public TenantRead getById(UUID tenantId) throws IOException, PermitApiError, PermitContextError {
         return this.get(tenantId.toString());
     }
 
+    /**
+     * Creates a new tenant.
+     *
+     * @param tenantData The TenantCreate object representing the tenant data.
+     * @return A TenantRead object representing the created tenant.
+     * @throws IOException           If an I/O error occurs during the HTTP request.
+     * @throws PermitApiError        If an error occurs in the Permit API.
+     * @throws PermitContextError    If there is an error in the Permit context.
+     */
     public TenantRead create(TenantCreate tenantData) throws IOException, PermitApiError, PermitContextError {
         ensureContext(ApiKeyLevel.ENVIRONMENT_LEVEL_API_KEY);
         String url = getTenantsUrl("");
@@ -132,6 +240,16 @@ public class TenantsApi extends BaseApi implements ITenantsApi {
         return this.<TenantRead>callApiAndParseJson(request, TenantRead.class);
     }
 
+    /**
+     * Updates an existing tenant.
+     *
+     * @param tenantKey  The key of the tenant to be updated.
+     * @param tenantData The TenantUpdate object representing the updated tenant data.
+     * @return A TenantRead object representing the updated tenant.
+     * @throws IOException           If an I/O error occurs during the HTTP request.
+     * @throws PermitApiError        If an error occurs in the Permit API.
+     * @throws PermitContextError    If there is an error in the Permit context.
+     */
     public TenantRead update(String tenantKey, TenantUpdate tenantData) throws IOException, PermitApiError, PermitContextError {
         ensureContext(ApiKeyLevel.ENVIRONMENT_LEVEL_API_KEY);
         String url = getTenantsUrl(String.format("/%s", tenantKey));
@@ -146,6 +264,14 @@ public class TenantsApi extends BaseApi implements ITenantsApi {
         return this.<TenantRead>callApiAndParseJson(request, TenantRead.class);
     }
 
+    /**
+     * Deletes a tenant.
+     *
+     * @param tenantKey The key of the tenant to be deleted.
+     * @throws IOException           If an I/O error occurs during the HTTP request.
+     * @throws PermitApiError        If an error occurs in the Permit API.
+     * @throws PermitContextError    If there is an error in the Permit context.
+     */
     public void delete(String tenantKey) throws IOException, PermitApiError, PermitContextError {
         ensureContext(ApiKeyLevel.ENVIRONMENT_LEVEL_API_KEY);
         String url = getTenantsUrl(String.format("/%s", tenantKey));
--- src/main/java/io/permit/sdk/api/UsersApi.java
+++ src/main/java/io/permit/sdk/api/UsersApi.java
@@ -34,11 +34,26 @@ interface IUsersApi {
     RoleAssignmentRead[] getAssignedRoles(@NotNull String userKey) throws IOException, PermitApiError, PermitContextError;
 }
 
+/**
+ * The UsersApi class provides methods for interacting with the Permit Users REST API.
+ */
 public class UsersApi extends BaseApi implements IUsersApi {
+    /**
+     * Constructs a new UsersApi instance with the specified OkHttpClient and PermitConfig.
+     *
+     * @param client The OkHttpClient instance to be used for making HTTP requests.
+     * @param config The PermitConfig instance that contains the SDK configuration.
+     */
     public UsersApi(OkHttpClient client, PermitConfig config) {
         super(client, config, LoggerFactory.getLogger(UsersApi.class));
     }
 
+    /**
+     * Constructs the URLs for users API endpoints.
+     *
+     * @param url The URL fragment.
+     * @return The formatted URL for the endpoint.
+     */
     private String getUsersUrl(String url) {
         return buildUrl(
                 String.format(
@@ -50,6 +65,16 @@ public class UsersApi extends BaseApi implements IUsersApi {
         );
     }
 
+    /**
+     * Retrieves a paginated result of users.
+     *
+     * @param page    The page number of the result set to retrieve.
+     * @param perPage The number of items per page.
+     * @return A PaginatedResultUserRead object representing the retrieved paginated result of users.
+     * @throws IOException           If an I/O error occurs during the HTTP request.
+     * @throws PermitApiError        If an error occurs in the Permit API.
+     * @throws PermitContextError    If there is an error in the Permit context.
+     */
     public PaginatedResultUserRead list(int page, int perPage) throws IOException, PermitApiError, PermitContextError {
         ensureContext(ApiKeyLevel.ENVIRONMENT_LEVEL_API_KEY);
         String url = getUsersUrl("");
@@ -71,14 +96,40 @@ public class UsersApi extends BaseApi implements IUsersApi {
         }
     }
 
+    /**
+     * Retrieves a paginated result of users with the default number of items per page.
+     *
+     * @param page The page number of the result set to retrieve.
+     * @return A PaginatedResultUserRead object representing the retrieved paginated result of users.
+     * @throws IOException           If an I/O error occurs during the HTTP request.
+     * @throws PermitApiError        If an error occurs in the Permit API.
+     * @throws PermitContextError    If there is an error in the Permit context.
+     */
     public PaginatedResultUserRead list(int page) throws IOException, PermitApiError, PermitContextError {
         return this.list(page, 100);
     }
 
+    /**
+     * Retrieves a paginated result of users with default pagination.
+     *
+     * @return A PaginatedResultUserRead object representing the retrieved paginated result of users.
+     * @throws IOException           If an I/O error occurs during the HTTP request.
+     * @throws PermitApiError        If an error occurs in the Permit API.
+     * @throws PermitContextError    If there is an error in the Permit context.
+     */
     public PaginatedResultUserRead list() throws IOException, PermitApiError, PermitContextError {
         return this.list(1);
     }
 
+    /**
+     * Retrieves a user by its key.
+     *
+     * @param userKey The key of theuser.
+     * @return A UserRead object representing the retrieved user.
+     * @throws IOException           If an I/O error occurs during the HTTP request.
+     * @throws PermitApiError        If an error occurs in the Permit API.
+     * @throws PermitContextError    If there is an error in the Permit context.
+     */
     public UserRead get(String userKey) throws IOException, PermitApiError, PermitContextError {
         ensureContext(ApiKeyLevel.ENVIRONMENT_LEVEL_API_KEY);
         String url = getUsersUrl(String.format("/%s", userKey));
@@ -91,14 +142,41 @@ public class UsersApi extends BaseApi implements IUsersApi {
         return this.<UserRead>callApiAndParseJson(request, UserRead.class);
     }
 
+    /**
+     * Retrieves a user by its key.
+     *
+     * @param userKey The key of the user.
+     * @return A UserRead object representing the retrieved user.
+     * @throws IOException           If an I/O error occurs during the HTTP request.
+     * @throws PermitApiError        If an error occurs in the Permit API.
+     * @throws PermitContextError    If there is an error in the Permit context.
+     */
     public UserRead getByKey(String userKey) throws IOException, PermitApiError, PermitContextError {
         return this.get(userKey);
     }
 
+    /**
+     * Retrieves a user by its ID.
+     *
+     * @param userId The ID of the user.
+     * @return A UserRead object representing the retrieved user.
+     * @throws IOException           If an I/O error occurs during the HTTP request.
+     * @throws PermitApiError        If an error occurs in the Permit API.
+     * @throws PermitContextError    If there is an error in the Permit context.
+     */
     public UserRead getById(UUID userId) throws IOException, PermitApiError, PermitContextError {
         return this.get(userId.toString());
     }
 
+    /**
+     * Creates a new user.
+     *
+     * @param userData The UserCreate object representing the user data.
+     * @return A UserRead object representing the created user.
+     * @throws IOException           If an I/O error occurs during the HTTP request.
+     * @throws PermitApiError        If an error occurs in the Permit API.
+     * @throws PermitContextError    If there is an error in the Permit context.
+     */
     public UserRead create(UserCreate userData) throws IOException, PermitApiError, PermitContextError {
         ensureContext(ApiKeyLevel.ENVIRONMENT_LEVEL_API_KEY);
         String url = getUsersUrl("");
@@ -113,6 +191,16 @@ public class UsersApi extends BaseApi implements IUsersApi {
         return this.<UserRead>callApiAndParseJson(request, UserRead.class);
     }
 
+    /**
+     * Updates an existing user.
+     *
+     * @param userKey  The key of the user to be updated.
+     * @param userData The UserUpdate object representing the updated user data.
+     * @return A UserRead object representing the updated user.
+     * @throws IOException           If an I/O error occurs during the HTTP request.
+     * @throws PermitApiError        If an error occurs in the Permit API.
+     * @throws PermitContextError    If there is an error in the Permit context.
+     */
     public UserRead update(String userKey, UserUpdate userData) throws IOException, PermitApiError, PermitContextError {
         ensureContext(ApiKeyLevel.ENVIRONMENT_LEVEL_API_KEY);
         String url = getUsersUrl(String.format("/%s", userKey));
@@ -127,6 +215,15 @@ public class UsersApi extends BaseApi implements IUsersApi {
         return this.<UserRead>callApiAndParseJson(request, UserRead.class);
     }
 
+    /**
+     * Creates a user or updates a user in place (if the user key already exists).
+     *
+     * @param userData The UserCreate object representing the user data to synchronize.
+     * @return A CreateOrUpdateResult object representing the result of the synchronization.
+     * @throws IOException           If an I/O error occurs during the HTTP request.
+     * @throws PermitApiError        If an error occurs in the Permit API.
+     * @throws PermitContextError    If there is an error in the Permit context.
+     */
     public CreateOrUpdateResult<UserRead> sync(UserCreate userData) throws IOException, PermitApiError, PermitContextError {
         if (userData.key == null) {
             throw new PermitApiError(
@@ -154,6 +251,16 @@ public class UsersApi extends BaseApi implements IUsersApi {
         }
     }
 
+    /**
+     * Creates a user or updates a user in place (if the user key already exists).
+     * This override uses the same User class used for permit.check() authorization checks.
+     *
+     * @param user The User object representing the user to synchronize.
+     * @return A CreateOrUpdateResult object representing the result of the synchronization.
+     * @throws IOException           If an I/O error occurs during the HTTP request.
+     * @throws PermitApiError        If an error occurs in the Permit API.
+     * @throws PermitContextError    If there is an error in the Permit context.
+     */
     public CreateOrUpdateResult<UserRead> sync(User user) throws IOException, PermitApiError, PermitContextError {
         UserCreate userData = new UserCreate(user.getKey());
         if (user.getEmail() != null) {
@@ -172,6 +279,14 @@ public class UsersApi extends BaseApi implements IUsersApi {
         return this.sync(userData);
     }
 
+    /**
+     * Deletes a user.
+     *
+     * @param userKey The key of the user to be deleted.
+     * @throws IOException           If an I/O error occurs during the HTTP request.
+     * @throws PermitApiError        If an error occurs in the Permit API.
+     * @throws PermitContextError    If there is an error in the Permit context.
+     */
     public void delete(String userKey) throws IOException, PermitApiError, PermitContextError {
         ensureContext(ApiKeyLevel.ENVIRONMENT_LEVEL_API_KEY);
         String url = getUsersUrl(String.format("/%s", userKey));
@@ -186,6 +301,17 @@ public class UsersApi extends BaseApi implements IUsersApi {
         }
     }
 
+    /**
+     * Assigns a role to a user in a specific tenant.
+     *
+     * @param userKey   The key of the user.
+     * @param roleKey   The key of the role to assign.
+     * @param tenantKey The key of the tenant.
+     * @return A RoleAssignmentRead object representing the assigned role.
+     * @throws IOException           If an I/O error occurs during the HTTP request.
+     * @throws PermitApiError        If an error occurs in the Permit API.
+     * @throws PermitContextError    If there is an error in the Permit context.
+     */
     public RoleAssignmentRead assignRole(String userKey, String roleKey, String tenantKey) throws IOException, PermitApiError, PermitContextError {
         ensureContext(ApiKeyLevel.ENVIRONMENT_LEVEL_API_KEY);
         String url = getUsersUrl(String.format("/%s/roles", userKey));
@@ -200,6 +326,16 @@ public class UsersApi extends BaseApi implements IUsersApi {
         return this.<RoleAssignmentRead>callApiAndParseJson(request, RoleAssignmentRead.class);
     }
 
+    /**
+     * Unassigns a role from a user in a specific tenant.
+     *
+     * @param userKey   The key of the user.
+     * @param roleKey   The key of the role to unassign.
+     * @param tenantKey The key of the tenant.
+     * @throws IOException           If an I/O error occurs during the HTTP request.
+     * @throws PermitApiError        If an error occurs in the Permit API.
+     * @throws PermitContextError    If there is an error in the Permit context.
+     */
     public void unassignRole(String userKey, String roleKey, String tenantKey) throws IOException, PermitApiError, PermitContextError {
         ensureContext(ApiKeyLevel.ENVIRONMENT_LEVEL_API_KEY);
         String url = getUsersUrl(String.format("/%s/roles", userKey));
@@ -216,6 +352,18 @@ public class UsersApi extends BaseApi implements IUsersApi {
         }
     }
 
+    /**
+     * Retrieves a paginated result of role assignments for a specific user and tenant.
+     *
+     * @param userKey   The key of the user.
+     * @param tenantKey The key of the tenant (optional, can be null).
+     * @param page      The page number of the result set to retrieve.
+     * @param perPage   The number of items per page.
+     * @return An array of RoleAssignmentRead objects representing the retrieved role assignments.
+     * @throws IOException           If an I/O error occurs during the HTTP request.
+     * @throws PermitApiError        If an error occurs in the Permit API.
+     * @throws PermitContextError    If there is an error in the Permit context.
+     */
     public RoleAssignmentRead[] getAssignedRoles(@NotNull String userKey, String tenantKey, int page, int perPage) throws IOException, PermitApiError, PermitContextError {
         ensureContext(ApiKeyLevel.ENVIRONMENT_LEVEL_API_KEY);
         String url = buildUrl(
@@ -247,14 +395,46 @@ public class UsersApi extends BaseApi implements IUsersApi {
         }
     }
 
+    /**
+     * Retrieves a paginated result of roles assigned to the user across all tenants.
+     * For example - if the user is Admin in tenant `green`, and Viewer in tenant `blue`,
+     * this override of getAssignedRoles will return both roles (will not filter on one tenant).
+     *
+     * @param userKey The key of the user.
+     * @param page    The page number of the result set to retrieve.
+     * @param perPage The number of items per page.
+     * @return An array of RoleAssignmentRead objects representing the retrieved role assignments.
+     * @throws IOException           If an I/O error occurs during the HTTP request.
+     * @throws PermitApiError        If an error occurs in the Permit API.
+     * @throws PermitContextError    If there is an error in the Permit context.
+     */
     public RoleAssignmentRead[] getAssignedRoles(@NotNull String userKey, int page, int perPage) throws IOException, PermitApiError, PermitContextError {
         return this.getAssignedRoles(userKey, null, page, perPage);
     }
 
+    /**
+     * Retrieves a paginated result of of roles assigned to the user across all tenants with the default number of items per page.
+     *
+     * @param userKey The key of the user.
+     * @param page    The page number of the result set to retrieve.
+     * @return An array of RoleAssignmentRead objects representing the retrieved role assignments.
+     * @throws IOException           If an I/O error occurs during the HTTP request.
+     * @throws PermitApiError        If an error occurs in the Permit API.
+     * @throws PermitContextError    If there is an error in the Permit context.
+     */
     public RoleAssignmentRead[] getAssignedRoles(@NotNull String userKey, int page) throws IOException, PermitApiError, PermitContextError {
         return this.getAssignedRoles(userKey, page, 100);
     }
 
+    /**
+     * Retrieves a paginated result of of roles assigned to the user across all tenants with the default pagination.
+     *
+     * @param userKey The key of the user.
+     * @return An array of RoleAssignmentRead objects representing the retrieved role assignments.
+     * @throws IOException           If an I/O error occurs during the HTTP request.
+     * @throws PermitApiError        If an error occurs in the Permit API.
+     * @throws PermitContextError    If there is an error in the Permit context.
+     */
     public RoleAssignmentRead[] getAssignedRoles(@NotNull String userKey) throws IOException, PermitApiError, PermitContextError {
         return this.getAssignedRoles(userKey, 1);
     }
